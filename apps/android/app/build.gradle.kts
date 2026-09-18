da0d1f7f8106f21d317eb276b8c25448fc51406d
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2026-09-18 15:03:20 +0300

    build(android): release 0.4.0 (version code 5)

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.bookharbor.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
